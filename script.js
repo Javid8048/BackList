fefa1490ecd1a861535b6e59687eaa6ce34c98f8
JAVID ASHWAQ F <112797329+Javid8048@users.noreply.github.com>
2023-11-14 09:03:54 +0000

    script.js

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+const btnScrollTo = document.querySelector(".btn--scroll-to")
+
+const header = document.querySelector(".header");
+const section1 = document.querySelector("#section--1");
+const cookieMessage = document.querySelector(".cookie-message");
+
+
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
@@ -18,8 +25,11 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener("click", openModal);
+})
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -28,4 +38,18 @@ document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
+});
+
+const cookie = document.createElement("div");
+cookie.innerHTML = "We use cookie for increasing performance <button class='btn cookie-message-btn'> Ok </button>";
+cookie.classList.add("cookie-message");
+header.append(cookie);
+// let cookieBtn = "<div class='cookie-message'> We use cookie for increasing performance <button class='btn'> Ok </button> </div>";
+// header.insertAdjacentHTML('afterbegin', cookieBtn);
+
+// Event handelers
+const cookieMessageBtnEvent = document.querySelector(".cookie-message-btn").addEventListener('click', () => cookie.classList.add("hidden"));
+btnScrollTo.addEventListener("click", ()=> {
+  window.scrollBy({left: 0, top: section1.getBoundingClientRect().y, behaviour: "smooth"})
+  section1.scrollIntoView({behaviour: "smooth"});
 });
