83e20ffcf9f1a9a3b008a336b3fc5c20dfb7d485
JAVID ASHWAQ F <112797329+Javid8048@users.noreply.github.com>
2023-11-15 06:58:37 +0000

    main

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
+
 // for (let i = 0; i < btnsOpenModal.length; i++)
-//   btnsOpenModal[i].addEventListener('click', openModal);
+// btnsOpenModal[i].addEventListener('click', openModal);
 btnsOpenModal.forEach(btn => {
   btn.addEventListener("click", openModal);
 })
@@ -47,9 +47,44 @@ header.append(cookie);
 // let cookieBtn = "<div class='cookie-message'> We use cookie for increasing performance <button class='btn'> Ok </button> </div>";
 // header.insertAdjacentHTML('afterbegin', cookieBtn);
 
+
+
 // Event handelers
 const cookieMessageBtnEvent = document.querySelector(".cookie-message-btn").addEventListener('click', () => cookie.classList.add("hidden"));
-btnScrollTo.addEventListener("click", ()=> {
-  window.scrollBy({left: 0, top: section1.getBoundingClientRect().y, behaviour: "smooth"})
-  section1.scrollIntoView({behaviour: "smooth"});
+btnScrollTo.addEventListener("click", () => {
+  section1.scrollIntoView({ behaviour: "smooth" })
+  // window.scrollBy({left: 0, top: section1.getBoundingClientRect().y, behaviour: "smooth"})
 });
+document.querySelector(".nav__links").addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.classList.contains("nav__link")) {
+    // const id = e.target.href.split("/").at(-1);
+    const id = document.querySelector(e.target.getAttribute("href"));
+    id.scrollIntoView({ behaviour: "smooth" });
+  }
+});
+
+const TabsContainer = document.querySelector(".operations__tab-container");
+const Tabs = document.querySelectorAll(".operations__tab");
+const TabsView = document.querySelectorAll(".operations__content");
+TabsContainer.addEventListener("click", e => {
+  const clicked = e.target.closest(".operations__tab");
+  if (!clicked) return;
+  console.log(clicked);
+  // clicked.dataset.myname         ("javid")    -- the data set can changed to lowercase if we even use camelcase
+  let no = clicked.dataset.tab;
+  console.log(no);
+  Tabs.forEach(t => t.classList.remove("operations__tab--active"));
+  TabsView.forEach(tv => tv.classList.remove("operations__content--active"))
+  document.querySelector(`.operations__tab--${no}`).classList.add("operations__tab--active");
+  document.querySelector(`.operations__content--${no}`).classList.add("operations__content--active");
+});
+
+const navLinks = document.querySelector(".nav__links");
+const navItem = document.querySelectorAll(".nav__item");
+const navLink = document.querySelectorAll(".nav__link");
+navLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+  })
+})
